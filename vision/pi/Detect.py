404933f6efc1db694770b7a72795cc9e693cf90e
5413-Laptop-2 <zortanproductions@gmail.com>
2024-01-20 12:20:33 -0500

    Added working vision code that runs on the Pi

---
+++ vision/pi/Detect.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+import robotpy_apriltag as apriltag
+import os
+import time
+from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer
+from ntcore import NetworkTableInstance, EventFlags
+
+def main():
+    tagSize = 0.17 # Tag size for competition is ~17 cm
+    focalCenterX = 0 # Currently unknown, pixels
+    focalCenterY = 0 # Currently unknown, pixels
+    focalLengthX = 0 # Currently unknown, pixels
+    focalLengthY = 0 # Currently unknown, pixels
+
+    # Experimental CameraServer Init Code
+    #CameraServer.enableLogging()
+
+    #
+    #refrence = UsbCamera()
+    #cap = cv2.VideoCapture(0)
+    camera = CameraServer.startAutomaticCapture("Stellar Sight", "/dev/video0")
+    camera.setResolution(1280, 720)
+    cvSink = CameraServer.getVideo()
+    output_stream = CameraServer.putVideo('Processed', 1280, 720)
+
+    input_img = np.zeros(shape=(1280, 720, 3), dtype=np.uint8)
+
+    time, input_img = cvSink.grabFrame(input_img)
+
+    if input_img.any():
+        print("I equal something!!!!!!!")
+    else:
+        print("I am nothing!!!!")
+
+    #if time == 0:
+        #print("We Error'd Out!")
+        #print(cvSink.getError())
+
+    while True:
+        continue
+        
+    
+
+    # Initialize the camera
+    #cap = cv2.VideoCapture(0)  # Use the camera at index 0 (usually the built-in camera)
+
+
+    # Create an AprilTag detector
+    #detector = apriltag.AprilTagDetector()
+    #detector.addFamily("tag36h11")
+    #config = apriltag.AprilTagPoseEstimator.Config(tagSize, focalLengthX, focalLengthY, focalCenterX, focalCenterY)
+    #estimator = apriltag.AprilTagPoseEstimator(config)
+    #prevId = False
+
+    #input_img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+
+    #while True:
+        # Capture a frame from the camera
+        #ret, frame = cap.read()
+
+        #time, input_img = cvSink.grabFrame(input_img)
+
+        
+        #if time == 0:
+            #print(cvSink.getError())
+            #break
+        
+        # Convert the frame to grayscale for AprilTag detection
+        #gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        
+        # Detect AprilTags in the grayscale frame
+        tags = detector.detect(gray)
+        
+        # Iterate through detected tags
+        for tag in tags:
+            # Draw the tag outline and ID on the frame
+            # tag.draw(frame)
+            
+            # Extract tag data
+            tag_id = tag.getId()
+            tag_family = tag.getFamily()
+            #pose = estimator.estimate(tag)
+
+            if prevId == tag_id:
+                os.system('clear')
+                continue
+            else:
+                os.system('cls' if os.name=='nt' else 'clear') # clears the console window
+                os.system(f"notify-send \"April tag detected: ID = {tag_id}\"" if os.name!='nt' else '') # Sends unix graphical notification if possible
+                #rint(f"Tag ID: {tag_id}") # Displays the tag_id in the console
+
+            #prevId = tag_id
+        
+        # Display the processed frame with detected tags
+        #cv2.imshow('AprilTags', frame)
+
+        
+        # Exit when the 'q' key is pressed
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
+    
+    # Release the camera and close OpenCV windows
+    #cap.release()
+    #cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
