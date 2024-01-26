7746816cb237ccdefc379b97886d7ae44c0c476c
Stellar Robotics Lap-2 <stellarrobotics@gmail.com>
2024-01-25 16:23:59 -0800

    Rotational Bug is fixed!

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -130,7 +130,7 @@ public class DriveSubsystem extends SubsystemBase {
     }
     */
     // minimum difference between current angle and target angle (allowing signed angle)
-    double angleDiff = (targetRobotAngle.getDegrees() - robotAngle + 180) % 360 - 180;
+    double angleDiff = (targetRobotAngle.getDegrees() - robotAngle + 540) % 360 - 180;
 
     System.out.println("robot angle: " + robotAngle + " target angle: " + targetRobotAngle.getDegrees() + "diff: " + angleDiff);
 
